9576e05989ee9389c6c180b28eb4c551e9b387ee
Brandprz22 <101594641+Brandprz22@users.noreply.github.com>
2024-10-14 12:54:43 -0600

    fibonacci

--- Practica_4/algorIte.c
+++ Practica_4/algorIte.c
@@ -16,3 +16,33 @@ void factorialIte(int n){
     printf("Factorial %d: %d\n",n,x);
 
 }
+
+// Function to print fibonacci series
+void printFib(int n) {
+    if (n < 1) {
+        printf("Invalid Number of terms\n");
+        return;
+    }
+
+    // When number of terms is greater than 0
+    int prev1 = 1;
+    int prev2 = 0;
+
+    printf("%d ", prev2);
+
+    // If n is 1, then we do not need to
+    // proceed further
+    if (n == 1)
+        return;
+
+    printf("%d ", prev1);
+
+    // Print 3rd number onwards using
+    // the recursive formula
+    for (int i = 3; i <= n; i++) {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
+        printf("%d ", curr);
+    }
+}
