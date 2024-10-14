89dccf1c8bf810923e5ff2f38430fc5c74ac7ae7
Brandprz22 <101594641+Brandprz22@users.noreply.github.com>
2024-10-14 13:03:53 -0600

    h

---
+++ Practica_4/algorRec.c
@@ -0,0 +1,16 @@
+#include <stdio.h> 
+#include <math.h> 
+#include <stdlib.h> 
+
+int factorialrec(int n){
+  if (n==0 || n==1)
+    return 1;
+  
+  return n*facrotialrec(n-1)
+}
+
+int fiborec(int n){
+  if(n==0)
+
+}
+
