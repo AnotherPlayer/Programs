9967dc430beb2b5af5041282335b4bb38e061eb8
Brandprz22 <101594641+Brandprz22@users.noreply.github.com>
2024-10-14 13:11:20 -0600

    YA ESTA:)

--- Practica_4/algorRec.c
+++ Practica_4/algorRec.c
@@ -10,10 +10,18 @@ int factorialrec(int n){
 }
 
 int fiborec(int n){
-  if(n==0)
-    return 0;
-  else (n==1)
-    return 1;
+  if(n<2)
+    return n;
   return factorial(n-1)+factorial(n-2);
 }
 
+
+void hanoi(int n, char from_rod, char to_rod, char aux_rod) {
+    if (n == 1) {
+        printf("Mueve el disco 1 de %c a %c\n", from_rod, to_rod);
+        return;
+    }
+    hanoi(n - 1, from_rod, aux_rod, to_rod);
+    printf("Mueve el disco %d de %c a %c\n", n, from_rod, to_rod);
+    hanoi(n - 1, aux_rod, to_rod, from_rod);
+}
