584e96544fd9ffe83c64ba499e56fb07c37b4d52
Brandprz22 <101594641+Brandprz22@users.noreply.github.com>
2024-10-21 12:35:33 -0600

    Actualizar algorIte.c
    listo está todo

--- Practica_4/algorIte.c
+++ Practica_4/algorIte.c
@@ -54,117 +54,36 @@ void fibonacci(int n) {
 
 //TORRES DE HAOI Iterativo
 
-// A structure to represent a stack 
-struct Stack { 
-unsigned capacity; 
-int top; 
-int *array; 
-}; 
-
-// function to create a stack of given capacity. 
-struct Stack* createStack(unsigned capacity){ 
-	
-	struct Stack* stack = (struct Stack*) malloc(sizeof(struct Stack)); 
-	stack -> capacity = capacity; 
-	stack -> top = -1; 
-	stack -> array = (int*) malloc(stack -> capacity * sizeof(int)); 
-	return stack; 
-} 
-
-// Stack is full when top is equal to the last index 
-int isFull(struct Stack* stack){ 
-	return (stack->top == stack->capacity - 1); 
-} 
-
-// Stack is empty when top is equal to -1 
-int isEmpty(struct Stack* stack){
-	return (stack->top == -1); 
-} 
-
-
-void push(struct Stack *stack, int item){ 
-	if (isFull(stack)) 
-		return; 
-	stack -> array[++stack -> top] = item; 
-} 
-
-// Function to remove an item from stack. It 
-// decreases top by 1 
-int pop(struct Stack* stack){ 
-	if (isEmpty(stack)) 
-		return INT_MIN; 
-	return stack -> array[stack -> top--]; 
-} 
-
-//Function to show the movement of disks 
-void moveDisk(char fromPeg, char toPeg, int disk){ 
-	printf("Move the disk %d from \'%c\' to \'%c\'\n", 
-		disk, fromPeg, toPeg); 
+// Función para mover un disco de una varilla a otra
+void moverDisco(char desde, char hacia) {
+    printf("Mover disco de %c a %c\n", desde, hacia);
 }
 
-// Function to implement legal movement between 
-// two poles 
-void moveDisksBetweenTwoPoles(struct Stack *src, struct Stack *dest, char s, char d){ 
+// Función iterativa para resolver las Torres de Hanói
+void torresDeHanoi(int n) {
+    char torre1 = 'A', torre2 = 'B', torre3 = 'C';
 
-	int pole1TopDisk = pop(src); 
-	int pole2TopDisk = pop(dest); 
+    // Si el número de discos es par, intercambiamos torre2 y torre3
+    if (n % 2 == 0) {
+        char temp = torre2;
+        torre2 = torre3;
+        torre3 = temp;
+    }
 
-	// When pole 1 is empty 
-	if (pole1TopDisk == INT_MIN){ 
-		push(src, pole2TopDisk); 
-		moveDisk(d, s, pole2TopDisk); 
-	} 
+    int totalMovimientos = (1 << n) - 1;  // 2^n - 1
 
-	// When pole2 pole is empty 
-	else if (pole2TopDisk == INT_MIN){ 
-		push(dest, pole1TopDisk); 
-		moveDisk(s, d, pole1TopDisk); 
-	} 
+    // Bucle para realizar cada movimiento
+    for (int i = 1; i <= totalMovimientos; i++) {
+        if (i % 3 == 1) {
+            moverDisco(torre1, torre3);  // Movimiento entre torre A y C
+        } else if (i % 3 == 2) {
+            moverDisco(torre1, torre2);  // Movimiento entre torre A y B
+        } else {
+            moverDisco(torre2, torre3);  // Movimiento entre torre B y C
+        }
+    }
+}
 
-	// When top disk of pole1 > top disk of pole2 
-	else if (pole1TopDisk > pole2TopDisk){ 
-		push(src, pole1TopDisk); 
-		push(src, pole2TopDisk); 
-		moveDisk(d, s, pole2TopDisk); 
-	} 
 
-	// When top disk of pole1 < top disk of pole2 
-	else{ 
-		push(dest, pole2TopDisk); 
-		push(dest, pole1TopDisk); 
-		moveDisk(s, d, pole1TopDisk); 
-	} 
-} 
-
-//Function to implement TOH puzzle 
-void tohIterative(int num_of_disks, struct Stack *src, struct Stack *aux, struct Stack *dest){ 
-	
-	int i, total_num_of_moves; 
-	char s = 'S', d = 'D', a = 'A'; 
-
-	//If number of disks is even, then interchange 
-	//destination pole and auxiliary pole 
-	if (num_of_disks % 2 == 0){ 
-		char temp = d; 
-		d = a; 
-		a = temp; 
-	} 
-	total_num_of_moves = pow(2, num_of_disks) - 1; 
-
-	//Larger disks will be pushed first 
-	for (i = num_of_disks; i >= 1; i--) 
-		push(src, i); 
-
-	for (i = 1; i <= total_num_of_moves; i++){ 
-		if (i % 3 == 1) 
-		moveDisksBetweenTwoPoles(src, dest, s, d); 
-
-		else if (i % 3 == 2) 
-		moveDisksBetweenTwoPoles(src, aux, s, a); 
-
-		else if (i % 3 == 0) 
-		moveDisksBetweenTwoPoles(aux, dest, a, d); 
-	} 
-} 
 
 
