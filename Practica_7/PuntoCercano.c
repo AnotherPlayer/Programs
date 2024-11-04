a3e590fe1e5885d267741f8decdd0e1208118697
Marco0903-IPN <marco.ramon0903@gmail.com>
2024-11-04 12:30:36 -0600

    Brandon gay PuntoCercano.c

--- Practica_7/PuntoCercano.c
+++ Practica_7/PuntoCercano.c
@@ -122,10 +122,10 @@ float closest(Point P[], int n)
 }
  
 // Driver program to test above functions
-int main()
+/*int main()
 {
     Point P[] = {{2, 3}, {12, 30}, {40, 50}, {5, 1}, {12, 10}, {3, 4}};
     int n = sizeof(P) / sizeof(P[0]);
     printf("The smallest distance is %f ", closest(P, n));
     return 0;
-}
+}*/
