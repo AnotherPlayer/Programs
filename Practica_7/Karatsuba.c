8a9f28b285f4ac4930ce6c4456359d536c1ac89a
Brandprz22 <101594641+Brandprz22@users.noreply.github.com>
2024-11-04 12:30:00 -0600

    fino amigos fine diria my friend

--- Practica_7/Karatsuba.c
+++ Practica_7/Karatsuba.c
@@ -84,3 +84,14 @@ long long int multiply(char *X, char *Y) {
     // Combine the three products
     return P1 * (1LL << (2 * sh)) + (P3 - P1 - P2) * (1LL << sh) + P2;
 }
+/*
+int main()
+{
+    printf ("%ld\n", multiply("1100", "1010"));
+    printf ("%ld\n", multiply("110", "1010"));
+    printf ("%ld\n", multiply("11", "1010"));
+    printf ("%ld\n", multiply("1", "1010"));
+    printf ("%ld\n", multiply("0", "1010"));
+    printf ("%ld\n", multiply("111", "111"));
+    printf ("%ld\n", multiply("11", "11"));
+}*/
