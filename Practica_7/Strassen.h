bc3e26b8da5fd7b20a1dd0cf96e5b2c9f5c15e1c
Hector David <hd.tetuan5002@gmail.com>
2024-11-04 12:39:34 -0600

    end .hs

---
+++ Practica_7/Strassen.h
@@ -0,0 +1,3 @@
+void print(const char* display, int matrix[][COL_1], int start_row, int start_column, int end_row, int end_column);
+void add_matrix(int matrix_A[][COL_1], int matrix_B[][COL_1], int matrix_C[][COL_1], int split_index);
+void multiply_matrix(int matrix_A[][COL_1], int matrix_B[][COL_2], int result_matrix[][COL_2]);
