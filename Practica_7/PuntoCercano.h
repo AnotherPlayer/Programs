a40584d67839bdd6cfa678a0d90019d964a409e1
Marco0903-IPN <marco.ramon0903@gmail.com>
2024-11-04 12:36:36 -0600

    Update PuntoCercano.h

---
+++ Practica_7/PuntoCercano.h
@@ -0,0 +1,8 @@
+int compareX(const void* a, const void* b);
+int compareY(const void* a, const void* b);
+float dist(Point p1, Point p2);
+float bruteForce(Point P[], int n);
+float min(float x, float y);
+float stripClosest(Point strip[], int size, float d);
+float closestUtil(Point P[], int n);
+float closest(Point P[], int n);
