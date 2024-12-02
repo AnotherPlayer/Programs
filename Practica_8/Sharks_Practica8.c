66bdd0ef8d0cc48557e781ba1cdd575048495bba
Hector David <hd.tetuan5002@gmail.com>
2024-12-02 12:39:42 -0600

    Pract8

--- Practica_8/Sharks_Practica8.c
+++ Practica_8/Sharks_Practica8.c
@@ -1,8 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+int cutRodRecur(int i, vector<int> &price) {
+    
+    // Base case
+    if(i==0)
+        return 0;
+    
+    int ans = 0;
+    
+    for (int j=1 ; j<=i ; j++) 
+        ans = max(ans, price[j-1]+cutRodRecur(i-j, price));
+        
+    return ans;
+
+}
+
+int cutRod(vector<int> &price) {
+
+    int n = price.size();
+    return cutRodRecur(n, price);
+    
+}
+
 int main(){
 
+    int price;
+    int[] price = {2,4,5,10,15,20};
 
-    
+    printf("Digite una longitd para la varilla");
+    scanf("%d",);
+
 }
