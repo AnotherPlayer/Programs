4a194700b5788ecdb22513e0f3586ff1df84182c
Hector David <hd.tetuan5002@gmail.com>
2024-12-02 12:50:38 -0600

    Practica_8 Done

--- Practica_8/Sharks_Practica8.c
+++ Practica_8/Sharks_Practica8.c
@@ -1,34 +1,37 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int cutRodRecur(int i, vector<int> &price) {
-    
-    // Base case
-    if(i==0)
+// Función para encontrar el valor máximo del rod cortado
+int cutRodRecur(int i, int price[], int n) {
+    // Caso base
+    if (i == 0)
         return 0;
-    
+
     int ans = 0;
-    
-    for (int j=1 ; j<=i ; j++) 
-        ans = max(ans, price[j-1]+cutRodRecur(i-j, price));
-        
+
+    // Encontrar el valor máximo para cada corte.
+    // Tomar el valor del rod de longitud j, y
+    // encontrar recursivamente el valor del rod de longitud (i-j).
+    for (int j = 1; j <= i; j++) 
+        ans = (ans > price[j-1] + cutRodRecur(i-j, price, n)) ? ans : price[j-1] + cutRodRecur(i-j, price, n);
+
     return ans;
-
 }
 
-int cutRod(vector<int> &price) {
-
-    int n = price.size();
-    return cutRodRecur(n, price);
-    
+// Función para encontrar el valor máximo del rod cortado
+int cutRod(int price[], int n) {
+    return cutRodRecur(n, price, n);
 }
 
-int main(){
+int main() {
 
-    int price;
-    int[] price = {2,4,5,10,15,20};
+    int price[] = {1, 5, 8, 9, 10, 17, 17, 20, 25, 30};
+    int n[] = {4,6,8,10};
 
-    printf("Digite una longitd para la varilla");
-    scanf("%d",);
+    for(int i=0 ; i<sizeof(n)/sizeof(n[0]) ; i++ ){
 
+        printf("El valor maximo para %d obtenido es de: %d\n",n[i], cutRod(price, n[i]));
+
+    }
+    return 0;
 }
